5506e564827b5f75aa0d49d5024218d52a42ef42
ritika-80 <ritikayadav7860@gmail.com>
2026-06-21 12:45:08 +0000

    container config done

---
+++ 01_Basic/Datatype.js
@@ -0,0 +1,20 @@
+"USE STRICT";// treat all JS code as newer version
+// alert(3 + 4); // we can use alert to show a message in the browser
+console.log(3+3)
+console.log("ritika" + " " + "sharma") // string concatenation
+
+let name = "ritika";
+let age = 25;
+let isLoggedIn = true;
+
+//number => 2 to power 53
+//bignit
+//String =>""
+//Boolean=> true/false
+//Null=>standalone value
+//Undefined
+//Symbol=> unique identifiers for objects
+
+console.log(typeof name);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
